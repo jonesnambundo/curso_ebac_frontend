3405436e8e25c91e23be7f184f4f09749384b3aa
jonesnambunndo@hotmail.com <jonesnambundo@mtel.inatel.br>
2023-11-21 09:48:08 -0300

    Commit inicial

---
+++ exerc.js
@@ -0,0 +1,9 @@
+"use strict";
+/*função de multiplicação */
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+/* função de saudação  */
+function saudacao(nome) {
+    return "Olá " + nome;
+}
---
+++ exerc.ts
@@ -0,0 +1,10 @@
+
+/*função de multiplicação */
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+/* função de saudação  */
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
